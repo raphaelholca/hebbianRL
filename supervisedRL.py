b756dab19ffcfb687e92b3507fcabf231753d2b7
raphaelholca <raphaelholca@gmail.com>
2014-10-08 16:27:36 +0200

    fixes the RPROP algorithm, now uses batch updates. Works correctly, achieves 99.7% correct on 0 vs. 1 classification.

--- supervisedRL.py
+++ supervisedRL.py
@@ -1,13 +1,15 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 import os
 os.chdir('../DPM/')
 import classes.input.mnist as mnist
 os.chdir('../RL/')
 
-# np.random.seed(12)
+np.random.seed(12)
 
 def sigmoid(x):
+	# print 'x : ' + str(np.round(x[0][0],0))
 	try:
 		return 1/(1+math.exp(-x))
 	except:
@@ -27,7 +29,7 @@ def evenLabels(images, labels):
 
 classes =  [0,1] # [4,9] #
 nClasses = len(classes)
-dataset = 'test' # 'train' #
+dataset = 'train' # 'train' #
 path = '../DPM/data-sets/MNIST'
 images, labels = mnist.read_images_from_mnist(classes = classes, dataset = dataset, path = path)
 ##? Normalize each image to the sum of its pixel values (due to feedforward inhibition in model)
@@ -37,17 +39,14 @@ images/=255. #normalize images to range [0,1]
 #even out label class distribution
 images, labels = evenLabels(images, labels)
 
-
 nImages = np.size(images,0)
-
-nDimActions, nDimStates = 1, np.size(images,1)
+nDimActions, nDimStates = 0, np.size(images,1)
 nInpNeurons = nDimStates + nDimActions + 1 # +1 for bias node
 nHidNeurons = 0
-nEpi = 10000
-nPrint = nEpi/10
+nEpi = 20
 nImages = np.size(images,0)
 npos, nneg = 1.2, 0.5
-dmin, dmax, dinit = 0.000001, 50.0, 1.0
+dmin, dmax, dinit = 0.000001, 50.0, 0.1
 d = np.ones(nInpNeurons)*dinit
 dW = np.zeros(nInpNeurons)
 W = np.random.rand(1, nInpNeurons)-0.5 #weight matrix
@@ -62,22 +61,29 @@ allActions = np.array([0, 1], dtype=int)
 allRewards = {'00':1, '01':0, '10':0, '11':1, '40':1, '41':0, '90':0, '91':1} #state+action
 
 #training
-tmpErr = 0.
 for e in range(nEpi):
-	idxState = np.random.randint(nImages) #pick a current state at random
-	cState = allStates[idxState, :]
-	cAction = np.random.choice(allActions) #choose a random action
-	concInput[:nDimStates+1] = cState #concatenate current state and action to be fed to the network
-	concInput[nDimStates+1:] = cAction
-	cReward = allRewards[str(labels[idxState])+str(cAction)] #get reward associated with state and action
+	batchErr = 0.
+	E = np.zeros(nInpNeurons)
+	dW = np.zeros(nInpNeurons)
+	for idxState in range(nImages):
+		cState = allStates[idxState, :]
+		# cAction = np.random.choice(allActions) #choose a random action
+		concInput[:nDimStates+1] = cState #concatenate current state and action to be fed to the network
+		##concInput[nDimStates+1:] = cAction
+		##cReward = allRewards[str(labels[idxState])+str(cAction)] #get reward associated with state and action
+		cReward = labels[idxState] ##
 
-	Q = sigmoid(np.dot(W,concInput[:,np.newaxis])) #compute Q-value/neuron activation
-	err = np.abs(Q-cReward)
-	dOut = (Q-cReward)*Q*(1-Q)*concInput #partial derivatives of the error with respect to the input weights
+		# print 'labels : ' + str(cReward)
+		Q = sigmoid(np.dot(W,concInput[:,np.newaxis])) #compute Q-value/neuron activation
+		# print 'Q : ' + str(np.round(Q,3))
 
-	E = dOut
+		batchErr += np.abs(Q-cReward)
+		E += (Q-cReward)*Q*(1-Q)*concInput #partial derivatives of the error with respect to the input weights
+
+	print 'batch err : ' + str(np.round(batchErr/nImages,5))
 	s = np.sign(prevE * E)
 
+	#compute weight update and adaptation
 	d[s==+1] *= npos
 	d[s==-1] *= nneg
 	np.clip(d, dmin, dmax)
@@ -88,39 +94,30 @@ for e in range(nEpi):
 
 	W -= dW
 	prevE = E
-	# W -= dOut*lr #gradient descent
-	tmpErr += err
-	if e%nPrint==0 and e!=0:
-		# print concInput
-		# print Q
-		# print cReward
-		# print
-		print np.round(tmpErr/nPrint, 2)
-		tmpErr = 0.
+	print
 
 
 #testing
-print
-totErr = 0.
-nEpiTest = 1000
-labErr = [0,0]
-for e in range(nEpiTest):
-	idxState = np.random.randint(nImages) #pick a current state at random
-	cState = allStates[idxState, :]
-	cAction = np.random.choice(allActions) #choose a random action
-	concInput[:nDimStates+1] = cState #concatenate current state and action to be fed to the network
-	concInput[nDimStates+1:] = cAction
+# print 'testing...'
+# dataset = 'test'
+# images, labels = mnist.read_images_from_mnist(classes = classes, dataset = dataset, path = path)
+# images/=255.
+# images, labels = evenLabels(images, labels)
+# nImages = np.size(images,0)
 
-	Q = sigmoid(np.dot(W,concInput[:,np.newaxis])) #compute Q-value/neuron activation
-	cReward = allRewards[str(labels[idxState])+str(cAction)] #get reward associated with state and action
+# allStates = np.ones((nImages, nDimStates+1)) #add 1 to input vector for bias node
+# allStates[:,:-1] = images
 
-	err = np.abs(Q-cReward)
-	totErr += err
-	if err > 0.01:
-		labErr[labels[idxState]]+=1
-print np.round(totErr/nEpiTest, 3)
-print labErr
-
+# totErr = 0.
+# countErr = 0.
+# for i in range(nImages):
+# 	Q = sigmoid(np.dot(W,allStates[i,:][:,np.newaxis]))
+# 	err = np.abs(Q-labels[i])
+# 	totErr += err
+# 	if err >= 0.1: countErr += 1.
+# print 'test error: ' + str(np.round(totErr/nImages,3))
+# print countErr
+# print 'test error count: ' + str(np.round(countErr/nImages,3))
 
 
 
