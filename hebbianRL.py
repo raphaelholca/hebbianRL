Fixes the softmax function to allow low temperature values. Creates plotting function to display dopa and ach release levels. Creates an option to sort weights according to their class when displaying them. Creates a function to quantify the selectivity of individual receptive fields. Working on the procedural learning step now...

hebbianRL.py
@@ -11,7 +11,7 @@ import support.plots as pl
 import support.classifier as cl
 import support.assessRF as rf
 import support.mnist as mnist
-import pdb
+import sys
 ex = reload(ex)
 pl = reload(pl)
 cl = reload(cl)
@@ -23,45 +23,47 @@ experimental variables
 classes (int) 	: class of the MNIST dataset to use to train the network
 rActions (str)	: for each class of MNIST, the action that is rewarded. '0' indicates a class that is never rewarded; '1' indicates a class that is always rewarded; chararcters (e.g., 'a', 'b', etc.) indicate the specific action that is rewarded.
 """
-classes 	= np.array([ 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ], dtype=int)
-rActions 	= np.array(['0','0','0','0','a','0','0','0','0','b'], dtype='|S1')
+# classes 	= np.array([ 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ], dtype=int)
+# rActions 	= np.array(['0','0','0','0','a','0','0','0','0','0'], dtype='|S1')
 
-# classes 	= np.array([ 4 , 5 , 6 , 7 , 8 , 9 ], dtype=int)
-# rActions 	= np.array(['1','0','0','0','0','0'], dtype='|S1')
+classes 	= np.array([ 4 , 5 , 6 , 7 , 8 , 9 ], dtype=int)
+rActions 	= np.array(['a','0','0','0','0','0'], dtype='|S1')
 
 # classes 	= np.array([ 4 , 7 , 9 ], dtype=int)
-# rActions 	= np.array(['a','b','c'], dtype='|S1')
+# rActions 	= np.array(['c','0','b'], dtype='|S1')
 
 # classes 	= np.array([ 4 , 9 ], dtype=int)
-# rActions 	= np.array(['a','b'], dtype='|S1')
+# rActions 	= np.array(['b','a'], dtype='|S1')
 
 """ parameters """
 nRun 		= 1				# number of runs
 nEpiCrit	= 10			# number of 'critical period' episodes in each run (episodes when reward is not required for learning)
 nEpiProc	= 3				# number of 'procedural learning' episodes (to initialize the action weights after critical period)
-nEpiAdlt	= 30			# number of 'adult' episodes in each run (episodes when reward is not required for learning)
+nEpiAdlt	= 0				# number of 'adult' episodes in each run (episodes when reward is not required for learning)
 A 			= 900			# image normalization constant
-runName 	= 'ach_dopa'	# name of the folder where to save results
+runName 	= 'proc'		# name of the folder where to save results
 dataset 	= 'test'		# MNIST dataset to use; legal values: 'test', 'train' ##use train for actual results
-nHidNeurons = 100			# number of hidden neurons
+nHidNeurons = 49			# number of hidden neurons
 lrCrit		= 0.01 			# learning rate during 'critica period' (pre-training, nEpiCrit)
-lrAdlt		= 0.001		# learning rate after the end of the 'critica period' (adult/training, nEpiAdlt)
+lrAdlt		= 0.01			# learning rate after the end of the 'critica period' (adult/training, nEpiAdlt)
 ach_bool	= True			# whether to use ACh signalling
-aHigh 		= 0.03			# learning rate increase for relevance signal (high ACh) outside of critical period
+aHigh 		= 0.05			# learning rate increase for relevance signal (high ACh) outside of critical period
 aLow		= 0 			# learning rate increase without relevant signal (no ACh)
 dopa_bool	= True			# whether to use dopa signalling
 dHigh 		= 0.01			# learning rate increase for unexpected reward (high dopamine) outside of critical period
-dNeut 		= 0.0			# learning rate increase for correct reward prediction (neutral dopamine)
-dLow 		= -0.01/2		# learning rate increase for incorrect reward prediction (low dopamine)
+dMid 		= dHigh/3.		# learning rate increase for correct reward prediction
+dNeut 		= 0.0			# learning rate increase for no reward, when none predicted
+dLow 		= -dHigh*4		# learning rate increase for incorrect reward prediction (low dopamine)
 nBatch 		= 20 			# mini-batch size
 classifier	= 'neuronClass'	# which classifier to use for performance assessment. Possible values are: 'neural', 'SVM', 'neuronClass'
 bestAction 	= False			# whether to take predicted best action (True) or take random actions (False)
-feedback	= True			# whether to feedback activation of classification neurons to hidden neurons
+feedback	= False			# whether to feedback activation of classification neurons to hidden neurons
 balReward	= False			# whether reward should sum to the same value for stim. that are always rewarded and stim. that are rewarded for specific actions
 showPlots	= False			# whether to display plots
-show_W_act	= True 			# whether to display W_act weights on the weight plots
+show_W_act	= True			# whether to display W_act weights on the weight plots
+sort 		= False			# whether to sort weights by their class when displaying
 target		= None 			# target digit (to be used to color plots). Use 'None' if not desired
-seed 		= 75#np.random.randint(1000) 				# seed of the random number generator
+seed 		= 771#np.random.randint(1000) 				# seed of the random number generator
 
 ##cannot take random actions (bestAction=False) when using feedback with ACh because taking random actions leads to incorrect/meaningless feedback that is still paired with learning (ACh signal). Should this be solved? How?
 
@@ -80,8 +82,10 @@ W_in_save = {}
 W_act_save = {}
 W_class_save = {}
 nClasses = len(classes)
-lActions = np.unique(rActions[np.logical_and(rActions!='0', rActions!='1')]) #legal actions
-if len(lActions)==0: lActions = np.array(['a']) #creates at least one action neuron in case of no paired action
+rActions_z = np.copy(rActions)
+rActions_z[np.logical_or(rActions=='0', rActions=='1')] = 'z'
+_, idx = np.unique(rActions_z, return_index=True)
+lActions = rActions_z[np.sort(idx)] #legal actions, with order maintained, with 'z' for all classes with '0' and '1'
 nEpiTot = nEpiCrit + nEpiProc + nEpiAdlt
 np.random.seed(seed)
 nImages = np.size(images,0)
@@ -96,8 +100,8 @@ for r in range(nRun):
 	lr = lrCrit
 	ach = np.zeros(nBatch)
 	dopa = np.zeros(nBatch)
-	W_in = np.random.random_sample(size=(nDimStates, nHidNeurons)) + 1.
-	W_act = np.random.random_sample(size=(nHidNeurons, nDimActions)) + 1.
+	W_in = np.random.random_sample(size=(nDimStates, nHidNeurons)) + 1.0
+	W_act = np.random.random_sample(size=(nHidNeurons, nDimActions))/20.
 	if trainNeuro: W_class = np.random.random_sample(size=(nHidNeurons, nClasses)) + 1.
 
 	for e in range(nEpiTot):
@@ -121,10 +125,17 @@ for r in range(nRun):
 			bActNeurons = np.ones_like(bActNeurons)*1e-4 #reset neuron activation
 			bActNeurons[np.arange(nBatch), ex.val2idx(bActions, lActions)]=1. #activate the action neuron corresponding to the action taken
 
-			if e >= nEpiCrit:
-				#initialize variables
-				if e == nEpiCrit: 
-					lr = lrAdlt
+			if e >= nEpiCrit and e < nEpiCrit + nEpiProc: #procedural learning
+				lr = 0.0
+				ach = np.ones(nBatch)*aLow
+				dopa = np.ones(nBatch)*dNeut
+
+				#compute reward and dopa signal
+				bReward = ex.compute_reward(bLabels, classes, bActions, rActions_z)
+				dopa[bReward==1] = dHigh
+
+			elif e >= nEpiCrit + nEpiProc: #perceptual learning (adult)
+				lr = lrAdlt
 				ach = np.ones(nBatch)*aLow
 				dopa = np.ones(nBatch)*dNeut
 
@@ -132,12 +143,12 @@ for r in range(nRun):
 				bReward = ex.compute_reward(bLabels, classes, bActions, rActions)
 				
 				#determine acetylcholine strength based on task involvement
-				if ach_bool: 
+				if ach_bool and e >= (nEpiCrit + nEpiProc): #no ach until after procedural training
 					ach[ex.labels2actionVal(bLabels, classes, rActions)!='0'] = aHigh			#stimulus involved in task
 			
 				#determine dopamine signal strength based on reward
 				if dopa_bool:
-					dopa[np.logical_and(bPredictActions==bActions, bReward==1)] = dHigh			#correct reward prediction
+					dopa[np.logical_and(bPredictActions==bActions, bReward==1)] = dMid			#correct reward prediction
 					dopa[np.logical_and(bPredictActions==bActions, bReward==0)] = dLow			#incorrect reward prediction
 					dopa[np.logical_and(bPredictActions!=bActions, bReward==0)] = dNeut			#correct no reward prediction
 					dopa[np.logical_and(bPredictActions!=bActions, bReward==1)] = dHigh			#incorrect no reward prediction
@@ -145,17 +156,20 @@ for r in range(nRun):
 					dopa[bReward== 2]											= dNeut			#always rewarded
 
 				#feedback from classification layer
-				if feedback and e >= (nEpiCrit + nEpiProc): bHidNeurons += ex.propL1(bActNeurons, ex.softmax(W_act, t=0.01).T)*100 ##feeding through softmax makes feedback even for all hidden neurons ##compare with Pieter Roelfsema's work
+				if feedback: bHidNeurons += ex.propL1(bActNeurons, ex.softmax(W_act, t=0.01).T)*100 ##feeding through softmax makes feedback even for all hidden neurons that project to the top neurons (i.e., feedback weights are either 1 or 0) ##compare with Pieter Roelfsema's work
+
+			# bHidNeurons = ex.softmax(bHidNeurons)
 
 			#update weights
 			if e < (nEpiCrit + nEpiProc): dW_in = ex.learningStep(bImages, ex.softmax(bHidNeurons), W_in, lr) #no neurmodulators before adult in L1
 			else: dW_in = ex.learningStep(bImages, ex.softmax(bHidNeurons), W_in, lr, ach=ach, dopa=dopa)
 			W_in += dW_in
 			W_in = np.clip(W_in,1.0,np.inf) #necessary if using negative lr
-			dW_act = ex.learningStep(ex.softmax(bHidNeurons), bActNeurons, W_act, lr, ach=ach, dopa=dopa)
-			W_act += dW_act
-			W_act = np.clip(W_act,1e-10,np.inf) #necessary if using negative lr
-			if trainNeuro: W_class += ex.learningStep(bHidNeurons, bClassNeurons, W_class, lrCrit)
+			if e >= nEpiCrit or True: 
+				dW_act = ex.learningStep(ex.softmax(bHidNeurons, t=0.001), bActNeurons, W_act, lr, ach=ach, dopa=dopa)
+				W_act += dW_act #only learn action weights after the end of the critical period
+				W_act = np.clip(W_act,1e-10,np.inf) #necessary if using negative lr
+			if trainNeuro: W_class += ex.learningStep(ex.softmax(bHidNeurons), bClassNeurons, W_class, lrCrit)
 		
 	#save weights
 	W_in_save[str(r).zfill(3)] = np.copy(W_in)
@@ -170,13 +184,14 @@ RFproba, _ = rf.hist(runName, W_in_save, classes, nDimStates, proba=False, show=
 #compute correct weight assignment in the action layer
 correct_W_act = 0.
 for k in W_act_save.keys():
-	correct_W_act += np.sum(np.argmax(RFproba[int(k)],1)==classes[np.argmax(W_act_save[k],1)])
+	# correct_W_act += np.sum(np.argmax(RFproba[int(k)],1)==classes[np.argmax(W_act_save[k],1)])
+	correct_W_act += np.sum(ex.labels2actionVal(np.argmax(RFproba[int(k)],1), classes, rActions_z) == lActions[np.argmax(W_act_save[k],1)])
 correct_W_act/=len(RFproba)
 
 #plot the weights
 if show_W_act: W_act_pass=W_act_save
 else: W_act_pass=None
-rf.plot(runName, W_in_save, RFproba, target, W_act=W_act_pass)
+rf.plot(runName, W_in_save, RFproba, target, W_act=W_act_pass, sort=sort)
 
 #assess classification performance with neural classifier or SVM 
 if classifier=='neural': cl.neural(runName, W_in_save, W_class_save, classes, rActions, nHidNeurons, nDimStates, A, dataset, show=showPlots)
@@ -186,7 +201,7 @@ if classifier=='neuronClass': cl.neuronClass(runName, W_in_save, classes, RFprob
 print '\ncorrect action weight assignment: \n' + str(correct_W_act) + ' out of ' + str(nHidNeurons)+'.0'
 
 #save data
-ex.savedata(runName, W_in_save, W_act_save, W_class_save, seed, classes, rActions, dataset, A, nEpiCrit, nEpiProc, nEpiAdlt, nHidNeurons, lrCrit, lrAdlt, ach_bool, aHigh, aLow, dopa_bool, dHigh, dNeut, dLow, nBatch, bestAction, feedback, classifier)
+ex.save_data(runName, W_in_save, W_act_save, W_class_save, seed, nRun, classes, rActions, dataset, A, nEpiCrit, nEpiProc, nEpiAdlt, nHidNeurons, lrCrit, lrAdlt, ach_bool, aHigh, aLow, dopa_bool, dHigh, dMid, dNeut, dLow, nBatch, bestAction, feedback, classifier)
 
 print '\nrun: '+runName
 

support/assessRF.py
@@ -1,10 +1,12 @@
 """ routine to classify and build a histogram of the distribution of the classes of the weights (RFs) of the representation  """
 
 import numpy as np
-import support.plots as pl
+import plots as pl
+import external as ex
 import matplotlib.pyplot as pyplot
 import pickle
 pl = reload(pl)
+ex = reload(ex)
 
 def hist(runName, W, classes, nDimStates, proba=False, show=True):
 	"""
@@ -60,10 +62,15 @@ def hist(runName, W, classes, nDimStates, proba=False, show=True):
 
 	return RFproba, RFclass
 
-def plot(runName, W, RFproba, target=None, W_act=None):
+def plot(runName, W, RFproba, target=None, W_act=None, sort=False):
 	print "ploting RFs..."
 	for i,r in enumerate(sorted(W.keys())):
 		print 'run: ' + str(i+1)
+		if sort: #sort weights according to their class
+			RFclass = np.argmax(RFproba[i],1)
+			sort_idx = np.array([x for (y,x) in sorted(zip(RFclass, np.arange(len(RFclass))), key=lambda pair: pair[0])])
+			W[r] = W[r][:,sort_idx] 
+			RFproba[i] 	= np.array([x for (y,x) in sorted(zip(RFclass, RFproba[i]), key=lambda pair: pair[0])])
 		target_pass=None
 		if target:
 			T_idx = np.argwhere(np.argmax(RFproba[i],1)==target)
@@ -76,9 +83,22 @@ def plot(runName, W, RFproba, target=None, W_act=None):
 		pyplot.savefig('output/' + runName + '/RFs/' +runName+ '_' + str(r).zfill(3))
 		pyplot.close(fig)
 
-def sharp():
-	#TODO
-	return
+def selectivity(W, RFproba, images, labels, classes):
+	
+	acti = ex.propL1(images, W, SM=False)
+	nNeurons = np.size(acti,1)
+	nClasses = len(classes)
+	best_neuron = np.argmax(acti, 1)
+	RFclass = np.argmax(RFproba,1)
+	select_neuron = np.zeros(nNeurons)
+	select_class = np.zeros(nClasses)
+	for n in range(nNeurons):
+		all_acti, _ = np.histogram(labels[best_neuron==n], bins=10, range=(0,9))
+		select_neuron[n] = float(all_acti[RFclass[n]])/np.sum(all_acti)
+	for i, c in enumerate(classes):
+		select_class[i] = np.mean(select_neuron[RFclass==c])
+
+	return select_class, select_neuron
 
 
 

support/external.py
@@ -40,12 +40,15 @@ def softmax(activ, vectorial=True, t=1.):
 	
 	#vectorial
 	if vectorial:
-		scale = np.clip(np.max(activ,1)-700, 0, np.inf)
-		tmpRND=np.random.rand(np.shape(activ)[0],np.shape(activ)[1])/100000
-		activ+=tmpRND #add a random offset to insure that there is only a single min
-		activ[activ==np.min(activ,1)[:,np.newaxis]] = np.clip(np.min(activ,1), -740+scale, np.inf)
-		activ-=tmpRND
-		return np.exp((activ-scale[:,np.newaxis])/t) / np.sum(np.exp((activ-scale[:,np.newaxis])/t), 1)[:,np.newaxis]
+		# scale = np.clip(np.max(activ,1)-700, 0, np.inf)
+		# tmpRND=np.random.rand(np.shape(activ)[0],np.shape(activ)[1])/100000
+		# activ+=tmpRND #add a random offset to insure that there is only a single min
+		# activ[activ==np.min(activ,1)[:,np.newaxis]] = np.clip(np.min(activ,1), -740+scale, np.inf)
+		# activ-=tmpRND
+		# return np.exp((activ-scale[:,np.newaxis])/t) / np.sum(np.exp((activ-scale[:,np.newaxis])/t), 1)[:,np.newaxis]
+
+		activ_norm = np.copy(activ - np.max(activ,1)[:,np.newaxis])
+		return np.exp((activ_norm)/t) / np.sum(np.exp((activ_norm)/t), 1)[:,np.newaxis]
 
 	#iterative
 	else:
@@ -174,7 +177,7 @@ def compute_reward(labels, classes, actions, rActions):
 
 	return reward
 
-def savedata(runName, W_in, W_act, W_class, seed, classes, rActions, dataset, A, nEpiCrit, nEpiProc, nEpiAdlt, nHidNeurons, lrCrit, lrAdlt, ach_bool, aHigh, aLow, dopa_bool, dHigh, dNeut, dLow, nBatch, bestAction, feedback, classifier):
+def save_data(runName, W_in, W_act, W_class, seed, nRun, classes, rActions, dataset, A, nEpiCrit, nEpiProc, nEpiAdlt, nHidNeurons, lrCrit, lrAdlt, ach_bool, aHigh, aLow, dopa_bool, dHigh, dMid, dNeut, dLow, nBatch, bestAction, feedback, classifier):
 	"""
 	Save passed data to file. Use pickle for weights and ConfigObj for the setting parameters 
 
@@ -199,30 +202,93 @@ def savedata(runName, W_in, W_act, W_class, seed, classes, rActions, dataset, A,
 
 	settingFile = ConfigObj()
 	settingFile.filename 			= 'output/' + runName + '/settings.txt'
-	settingFile['runName'] 			= runName
-	settingFile['seed'] 			= seed
 	settingFile['classes'] 			= list(classes)
 	settingFile['rActions'] 		= list(rActions)
+	settingFile['nRun'] 			= nRun
 	settingFile['nEpiCrit']			= nEpiCrit
 	settingFile['nEpiProc']			= nEpiProc
 	settingFile['nEpiAdlt']			= nEpiAdlt 
+	settingFile['A'] 				= A
+	settingFile['runName'] 			= runName
+	settingFile['dataset'] 			= dataset
 	settingFile['nHidNeurons'] 		= nHidNeurons
-	settingFile['ach_bool'] 		= ach_bool
-	settingFile['dopa_bool'] 		= dopa_bool
-	settingFile['bestAction'] 		= bestAction
-	settingFile['feedback'] 		= feedback
 	settingFile['lrCrit']			= lrCrit
 	settingFile['lrAdlt']			= lrAdlt
+	settingFile['ach_bool'] 		= ach_bool
 	settingFile['aHigh'] 			= aHigh
 	settingFile['aLow'] 			= aLow
+	settingFile['dopa_bool'] 		= dopa_bool
 	settingFile['dHigh'] 			= dHigh
+	settingFile['dMid'] 			= dMid
 	settingFile['dNeut'] 			= dNeut
 	settingFile['dLow'] 			= dLow
-	settingFile['classifier'] 		= classifier
-	settingFile['dataset'] 			= dataset
 	settingFile['nBatch'] 			= nBatch
-	settingFile['A'] 				= A
+	settingFile['classifier'] 		= classifier
+	settingFile['bestAction'] 		= bestAction
+	settingFile['feedback'] 		= feedback
+	settingFile['seed'] 			= seed
+	
 	settingFile.write()
+
+def load_data(runs):
+	"""
+	Loads data from files for specified runs
+
+	Args:
+		runs (dict): dictionary of dictionaries of the runs to load from files
+
+	returns:
+		dict: orinigal dictionary filled with data loaded from file
+	"""
+
+	for k in runs.keys():
+		runName = k
+		datapath = '../output/' + runName
+
+		pFile = open('../output/' + runName + '/W_in', 'r')
+		runs[k]['W_in'] = pickle.load(pFile)
+		pFile.close()
+
+		pFile = open('../output/' + runName + '/W_act', 'r')
+		runs[k]['W_act'] = pickle.load(pFile)
+		pFile.close()
+
+		pFile = open('../output/' + runName + '/classResults', 'r')
+		runs[k]['classResults'] = pickle.load(pFile)
+		pFile.close()
+
+		pFile = open('../output/' + runName + '/RFclass', 'r')
+		runs[k]['RFclass'] = pickle.load(pFile)
+		pFile.close()
+
+		settingFile = ConfigObj(datapath+'/settings.txt')
+
+		runs[k]['runName'] 			= runName
+		runs[k]['classes'] 			= map(int, settingFile['classes'])
+		runs[k]['rActions'] 		= settingFile['rActions']
+		runs[k]['nEpiCrit'] 		= int(settingFile['nEpiCrit'])
+		runs[k]['nEpiProc'] 		= int(settingFile['nEpiProc'])
+		runs[k]['nEpiAdlt'] 		= int(settingFile['nEpiAdlt'])
+		runs[k]['nHidNeurons'] 		= int(settingFile['nHidNeurons'])
+		runs[k]['ach_bool'] 		= conv_bool(settingFile['ach_bool'])
+		runs[k]['dopa_bool'] 		= conv_bool(settingFile['dopa_bool'])
+		runs[k]['bestAction'] 		= conv_bool(settingFile['bestAction'])
+		runs[k]['feedback'] 		= conv_bool(settingFile['feedback'])
+		runs[k]['lrCrit'] 			= float(settingFile['lrCrit'])
+		runs[k]['lrAdlt'] 			= float(settingFile['lrAdlt'])
+		runs[k]['aHigh'] 			= float(settingFile['aHigh'])
+		runs[k]['aLow']				= float(settingFile['aLow'])
+		runs[k]['dHigh'] 			= float(settingFile['dHigh'])
+		runs[k]['dMid'] 			= float(settingFile['dMid'])
+		runs[k]['dNeut'] 			= float(settingFile['dNeut'])
+		runs[k]['dLow'] 			= float(settingFile['dLow'])
+		runs[k]['classifier'] 		= settingFile['classifier']
+		runs[k]['dataset'] 			= settingFile['dataset']
+		runs[k]['nRun'] 			= int(settingFile['nRun'])
+		runs[k]['nBatch'] 			= int(settingFile['nBatch'])
+		runs[k]['A'] 				= float(settingFile['A'])
+
+		runs[k]['nClasses'] = len(runs[k]['classes'])
 
 def checkdir(runName, OW_bool=True):
 	"""
@@ -357,7 +423,20 @@ def computeCM(classResults, labels_test, classes):
 			confusMatrix[ilabel, iclassif] = float(classifiedAs)/overTot
 	return confusMatrix
 
+def conv_bool(bool_str):
+	"""
+	Converts a string ('True', 'False') value to boolean (True, False)
 
+	Args:
+		bool_str (str): string to convert
+
+	returns:
+		bool: boolean value of the string
+	"""
+
+	if bool_str=='True': return True
+	elif bool_str=='False': return False
+	else: return None
 
 
 

support/plots.py
@@ -1,4 +1,4 @@
-""" plottting functions for the hebbian network and neural classifier """
+""" support plottting functions """
 
 import numpy as np
 import matplotlib as mpl
@@ -7,9 +7,12 @@ import matplotlib.pyplot as plt
 """ initialize color maps """
 NUM_COLORS = 9
 my_blues = [plt.get_cmap('YlGnBu')(1.*i/NUM_COLORS) for i in range(NUM_COLORS)]
+my_reds = [plt.get_cmap('YlOrRd')(1.*i/NUM_COLORS) for i in range(NUM_COLORS)]
 
-def plotRF(W, target=None, W_act=None):
-	""" plot weights """
+def plotRF(W, target=None, W_act=None, cmap='Blues'):
+	""" 
+	plots of the weights, with superimposed colouring for target digit and L2 weights 
+	"""
 	#plot parameters
 	nHidNeurons = np.size(W,1)
 	nDimStates = 784
@@ -26,8 +29,8 @@ def plotRF(W, target=None, W_act=None):
 	fig, ax = plt.subplots(figsize=(h,v))
 	for i in range(np.size(W,1)):
 		plt.subplot(v,h,i+1)
-		if type(target)!=type(None):
-			plt.imshow(target[i], cmap='Blues', vmin=0., vmax=3, extent=(0,28,0,28))
+		if type(target)!=type(None) and target[i]!=0:
+			plt.imshow(target[i], cmap=cmap, vmin=0., vmax=3, extent=(0,28,0,28))
 		plt.imshow(np.reshape(W[:nDimStates,i], (28,28)), interpolation='nearest', cmap=cmap_trans, extent=(0,28,0,28))
 		if type(W_act)!=type(None):
 			plt.imshow(W_act[i,:][:,np.newaxis], interpolation='nearest', cmap='binary', extent=(28,30,0,28))
@@ -89,6 +92,9 @@ def plotCM(confusMatrix, classes):
 	return fig
 
 def plotHist(h, bins, h_err=None):
+	"""
+	plots the histogram of receptive field class distribution
+	"""
 	fig, ax = plt.subplots(figsize=(1+0.5*len(h),3))
 	Xs = np.arange(len(h))
 	y_max = np.ceil(np.sum(h))
@@ -109,6 +115,70 @@ def plotHist(h, bins, h_err=None):
 	plt.tight_layout()
 
 	return fig
+
+def plotBar(levels, xlabels, ylabel, fsize, color):
+	"""
+	plots the different values of dopamine used during training
+	"""
+
+	c = np.where(color=='blue', my_blues[6], my_reds[5])
+
+	#values
+	levels[levels==0]+=1e-5
+	Xs = np.arange(len(levels))
+
+	#plot
+	fig, ax = plt.subplots(figsize=fsize)
+	ax.bar(Xs, levels, color=c, width=0.8)
+	
+
+	#plot parameters
+	fig.patch.set_facecolor('white')
+	ax.spines['right'].set_visible(False)
+	ax.spines['top'].set_visible(False)
+	ax.set_xticks(Xs+0.5)
+	ax.set_xticklabels(xlabels, rotation=70)
+	ax.set_yticks([-0.02, 0.0, 0.02, 0.04])
+	ax.set_ylim(-0.03,0.05)
+	ax.tick_params(axis='both', which='major', direction='out', labelsize=17)
+	ax.xaxis.set_ticks_position('bottom')
+	ax.yaxis.set_ticks_position('left')
+	ax.set_ylabel(ylabel, fontsize=18)
+	plt.tight_layout()
+
+	return fig
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 