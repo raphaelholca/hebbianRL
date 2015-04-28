583fef1d195e972f7f5c4c3407428e0efefea973
raphaelholca <raphaelholca@gmail.com>
2015-04-28 12:30:57 +0200

    updates changes to launch.py

--- launch.py
+++ launch.py
@@ -14,16 +14,16 @@ classes (int) 	: class of the MNIST dataset to use to train the network
 rActions (str)	: for each class of MNIST, the action that is rewarded. Capital letters indicates a class that is paired with ACh release.
 """
 
-# classes 	= np.array([ 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ], dtype=int)
-# rActions 	= np.array(['a','b','c','d','e','f','g','h','i','j'], dtype='|S1')
+classes 	= np.array([ 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ], dtype=int)
+rActions 	= np.array(['a','b','c','d','e','f','g','h','i','j'], dtype='|S1')
 
 # classes 	= np.array([ 3,  4 , 5 , 7 , 8 , 9 ], dtype=int)
 # rActions 	= np.array(['0','B','0','0','0','0'], dtype='|S1')
 # rActions 	= np.array(['a','B','c','d','e','f'], dtype='|S1')
 # rActions 	= np.array(['a','b','c','d','e','f'], dtype='|S1')
 
-classes 	= np.array([ 4 , 7 , 9 ], dtype=int)
-rActions 	= np.array(['a','b','c'], dtype='|S1')
+# classes 	= np.array([ 4 , 7 , 9 ], dtype=int)
+# rActions 	= np.array(['a','b','c'], dtype='|S1')
 
 # classes 	= np.array([ 4 , 9 ], dtype=int)
 # rActions 	= np.array(['a','b'], dtype='|S1')
@@ -37,16 +37,16 @@ kwargs = {
 'nEpiDopa'		: 3					,# number of 'adult' episodes in each run (episodes when reward is not required for learning)
 't'				: 0.001 			,# temperature of the softmax function (t<<1: strong competition; t>=1: weak competition)
 'A' 			: 1.2				,# input normalization constant. Will be used as: (input size)*A; for images: 784*1.2=940.8
-'runName' 		: 'dopa+dLow'		,# name of the folder where to save results
+'runName' 		: 'dopa_all_2'		,# name of the folder where to save results
 'dataset'		: 'train'			,# MNIST dataset to use; legal values: 'test', 'train' ##use train for actual results
-'nHidNeurons'	: 25				,# number of hidden neurons
+'nHidNeurons'	: 49				,# number of hidden neurons
 'lrCrit'		: 0.005 			,# learning rate during 'critica period' (pre-training, nEpiCrit)
 'lrAdlt'		: 0.005				,# learning rate after the end of the 'critica period' (adult/training, nEpiAch and nEpiDopa)
 'aHigh' 		: 0.0 				,# learning rate increase for relevance signal (high ACh) outside of critical period
-'dHigh' 		: 0.0 				,# learning rate increase for unexpected reward
-'dMid' 			: 0.1 				,# learning rate increase for correct reward prediction
-'dNeut' 		: 0.0				,# learning rate increase for correct no reward prediction
-'dLow' 			: -2.0				,# learning rate increase for incorrect reward predictio
+'dHigh' 		: 5.0 				,# learning rate increase for unexpected reward
+'dMid' 			: 0.2 				,# learning rate increase for correct reward prediction
+'dNeut' 		: -0.2				,# learning rate increase for correct no reward prediction
+'dLow' 			: -1.0				,# learning rate increase for incorrect reward predictio
 'nBatch' 		: 20 				,# mini-batch size
 'classifier'	: 'actionNeurons'	,# which classifier to use for performance assessment. Possible values are: 'actionNeurons', 'SVM', 'neuronClass'
 'SVM'			: False				,# whether to use an SVM or the number of stimuli that activate a neuron to determine the class of the neuron
