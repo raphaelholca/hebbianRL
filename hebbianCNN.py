1ea7c474e73bf8a64862a34007e7f094f6c396e7
raphaelholca <raphaelholca@gmail.com>
2015-06-18 12:33:42 +0200

    cleans up and rearrange code

--- hebbianCNN.py
+++ hebbianCNN.py
@@ -173,12 +173,12 @@ classes 	= np.array([ 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ], dtype=int)
 nClasses = len(classes)
 
 runName 			= 't_1'
-nEpi 				= 1
+nEpi 				= 10
 nCrit 				= 6
-DA 					= True
+DA 					= False
 A 					= 200.
 lr 					= 1e-5
-dataset 			= 'train'
+dataset 			= 'test'
 nBatch 				= 196  #196 112 49
 L1_mapNum 			= 12
 L1_conv_filterSide	= 5
@@ -189,15 +189,17 @@ seed 				= 970#np.random.randint(1000) #970
 np.random.seed(seed)
 print '\n' + 'run name: ' + runName + ' -- seed: ' + str(seed) + '\n'
 
-""" load and pre-process data """
+""" load and pre-process images """
 pad_size = (L1_conv_filterSide-1)/2
 imPath = '/Users/raphaelholca/Documents/data-sets/MNIST'
 images, labels = load_images(classes, dataset, imPath, pad_size)
+dataset_test='test' if dataset=='train' else 'train'
+images_test, labels_test = load_images(classes, dataset_test, imPath, pad_size)
 
 ##reduce size of training set
-images, labels = shuffle_images(images, labels)
-images = images[:10000, :,:]
-labels = labels[:10000]
+# images, labels = shuffle_images(images, labels)
+# images = images[:10000, :,:]
+# labels = labels[:10000]
 ##
 
 ##duplicate size of test dataset to test effect of size
@@ -210,33 +212,21 @@ labels = labels[:10000]
 # labels = labels_
 ##
 
-if dataset=='train':
-	images_test, labels_test = load_images(classes, 'test', imPath, pad_size)
-else: 
-	images_test, labels_test = load_images(classes, 'train', imPath, pad_size)
-step = labels_test.shape[0]/100
-images_test_short = images_test[::step]
-labels_test_short = labels_test[::step]
-
 """ initialize variables """
-#input
 images_num = np.size(images, 0)
 images_side = np.size(images,2)
-
-#layer 1 (convolutional feature maps)
 L1_conv_neuronNum = (images_side-L1_conv_filterSide+1)**2
 L1_conv_mapSide = int(np.sqrt(L1_conv_neuronNum))
 L1_subs_mapSide = L1_conv_mapSide/2
-
 size_params = {'L1_conv_neuronNum':L1_conv_neuronNum, 'L1_conv_filterSide':L1_conv_filterSide, 'L1_conv_mapSide':L1_conv_mapSide, 'L1_mapNum':L1_mapNum, 'L1_subs_mapSide':L1_subs_mapSide}
 
-#weights
-if False: #load pre-trained weights
+""" initialize weights """
+if False: #load pre-trained weights from file
 	# f = open('w_corners', 'r')
 	# L1_conv_W = pickle.load(f)
 	# f.close()
 
-	f = open('weights_long', 'r')
+	f = open('weights', 'r')
 	weights = pickle.load(f)
 	f.close()
 
@@ -244,27 +234,27 @@ if False: #load pre-trained weights
 	L1_subs_W = weights['L1_subs_W']
 	L2_feedf_W = weights['L2_feedf_W']
 	L3_class_W = weights['L3_class_W']
-else:
+else: #random initialization
 	L1_conv_W = np.random.random_sample(size=(L1_conv_filterSide**2, L1_mapNum)) + A/(L1_conv_filterSide**2) + 2.5 ##0.5
 	L1_subs_W = np.random.random_sample(size=((L1_subs_mapSide**2)*L1_mapNum, (L1_subs_mapSide**2)*L1_mapNum)) + 1.5 ##for now, no trainable weights between cMaps and sMaps...
 	L2_feedf_W = np.random.random_sample(size=((L1_subs_mapSide**2)*L1_mapNum, L2_feedf_neuronNum))/1000 + float(L1_subs_mapSide**2)/((L1_subs_mapSide**2)*L1_mapNum) + 0.6
 	L3_class_W = (np.random.random_sample(size=(L2_feedf_neuronNum, L3_class_neuronNum))/1000+1.0)/L2_feedf_neuronNum
 
-imcount = 0
-
 """ training network """
 print "training network..."
+imcount = 0
 for e in range(nEpi):
 	print '\ntrain epi: ' + str(e+1) + '/' + str(nEpi)
 	rndImages, rndLabels = shuffle_images(images, labels)
 
-	correct = 0.
+	correct_train = 0.
 	last_neuron_class = np.zeros((L2_feedf_neuronNum, L3_class_neuronNum))
 	dopa_save = []
 
 	pbar_epi = ProgressBar()
 	for i in pbar_epi(range(rndImages.shape[0])):
 		imcount+=1
+
 		#get input to the convolutional filter
 		input_conv = np.zeros((L1_conv_neuronNum, L1_conv_filterSide**2))
 		input_conv = get_conv_input(rndImages[i,:,:], input_conv, L1_conv_filterSide)
@@ -285,11 +275,9 @@ for e in range(nEpi):
 		FF_lin_noise = ex.softmax(FF_lin_noise, t=0.01)
 		FF_lin = ex.softmax(FF_lin, t=0.01)
 
-		dopa = None
-
 		#activate classification layer
 		class_lin = ex.propL1(FF_lin, L3_class_W, SM=True, t=0.001)
-
+		
 		if DA and e>=nCrit: #DOPA
 			#activate classification layer with noise
 			class_lin_noise = ex.propL1(FF_lin_noise, L3_class_W, SM=True, t=0.001)
@@ -302,17 +290,22 @@ for e in range(nEpi):
 			# dopa = ex.compute_dopa([np.argmax(class_lin)], [np.argmax(class_lin_noise)], reward, dHigh=2.0, dMid=1.00, dNeut=-0.02, dLow=-0.5) #testing parameters for convolutional layer
 
 			dopa_save.append(dopa[0])
+		else: dopa = None
 
 		last_neuron_class[np.argmax(FF_lin), np.argwhere(rndLabels[i]==classes)] += 1
 
 		# learn weights...
 		if imcount<60000:
+			still_training = True
 			for b in range(L1_conv_neuronNum/nBatch):
 				#...of the convolutional matrices
 				dW_conv = ex.learningStep(input_conv[b*nBatch:(b+1)*nBatch-1, :], FM_lin[b*nBatch:(b+1)*nBatch-1, :], L1_conv_W, lr=lr*0.1, disinhib=dopa)
 				# if e>= 8: dW_conv[:,[0,2,3,4,7,8,11]] = np.zeros((L1_conv_filterSide**2,7)) ##prevents learning of 1 weight...
 				L1_conv_W += dW_conv
 				L1_conv_W = np.clip(L1_conv_W, 1e-10, np.inf)
+		elif still_training: 
+			print '** stopped conv filter training **'
+			still_training = False
 
 		#...of the feedforward layer
 		dW_FF = ex.learningStep(SSM_lin, FF_lin_noise, L2_feedf_W, lr=lr*3600, disinhib=dopa)
@@ -324,7 +317,7 @@ for e in range(nEpi):
 		
 		if np.argmax(class_lin) == ex.label2idx(classes, [rndLabels[i]]):
 			dW_class *= 0.75
-			correct+=1
+			correct_train+=1
 		else:
 			dW_class *= -0.5
 
@@ -333,22 +326,27 @@ for e in range(nEpi):
 
 		if np.isnan(L3_class_W).any(): import pdb; pdb.set_trace()
 
-	print 'train error: ' + str(np.round((1.-correct/rndImages.shape[0])*100,2)) +'%'
+	print 'train error: ' + str(np.round((1.-correct_train/rndImages.shape[0])*100,2)) +'%'
 
+	step = labels_test.shape[0]/1005
+	rndImages_test, rndLabels_test = shuffle_images(images_test, labels_test)
+	images_test_short = rndImages_test[::step]
+	labels_test_short = rndLabels_test[::step]
 	correct_test = 0.
 	for i in range(images_test_short.shape[0]):
 		classif = propagate(images_test_short[i,:,:], L1_conv_W, L2_feedf_W, L3_class_W, A, 0.01, size_params)
 		if classif == labels_test_short[i]: correct_test += 1.
-	print 'test error: ' + str(np.round((1.-correct_test/images_test_short.shape[0])*100,2)) +'%'
+	print 'approx. test error: ' + str(np.round((1.-correct_test/images_test_short.shape[0])*100,2)) +'%'
 
 	correct_Wout = np.sum(np.argmax(last_neuron_class,1)==np.argmax(L3_class_W,1))
 	print 'correct W_out assignment: ' + str(correct_Wout) + '/' + str(L2_feedf_neuronNum)
 
 """ test network """
 print '\ntest epi'
-step = 8920/images_test.shape[0]
+step = images_test.shape[0]/8920
 images_test=images_test[::step,:,:]
 labels_test=labels_test[::step]
+print labels_test.shape
 
 correct = 0.
 pbar_epi = ProgressBar()
