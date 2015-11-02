3734b209ced170c7b95b37fff0c083922e2f3bb6
Raphael Holca <raphaelholca@gmail.com>
2015-11-02 17:02:35 +0100

    actual first commit of read_pypet.py

--- support/read_pypet.py
+++ support/read_pypet.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pypet
 
-# folder_path = '../output/pypet/'
-folder_path = '/Users/raphaelholca/Mountpoint/hebbianRL/output/dopa_L1_run3/'
+folder_path = '../output/normal_e_greedy_xplr/'
+# folder_path = '/Users/raphaelholca/Mountpoint/hebbianRL/output/same_DA_long_3/'
 
-traj_name = 'dMid'
+traj_name = 'xplr'
 traj = pypet.load_trajectory(traj_name, filename=folder_path + 'perf.hdf5', force=True)
 traj.v_auto_load = True
 
@@ -25,7 +25,9 @@ param_traj = traj.f_get_explored_parameters()
 param = {}
 for k in param_traj:
 	if k[11:] != 'runName':
-		param[k[11:]] = np.array(param_traj[k].f_get_range())[ok_runs]
+		xplr_values = np.array(param_traj[k].f_get_range())[ok_runs]
+		if len(np.unique(xplr_values)) >1:
+			param[k[11:]] = xplr_values
 
 arg_best = np.argmax(p_W_act)
 
@@ -40,10 +42,12 @@ for k in param.keys():
 
 keys = param.keys()
 for ik in range(len(keys)):
+	if len(keys)==1: ik=-1
 	for k in keys[ik+1:]:
 		others = keys[:]
-		others.remove(keys[ik])
-		others.remove(k)
+		if len(keys)>1: 
+			others.remove(keys[ik])
+			others.remove(k)
 		
 		mask = np.ones_like(param[k], dtype=bool)
 		if len(param)>2:
@@ -55,8 +59,8 @@ for ik in range(len(keys)):
 
 		fig = plt.figure()
 		fig.patch.set_facecolor('white')
-		plt.scatter(pX, pY, c=rC, cmap='CMRmap', vmin=0.0, vmax=1.0, s=5000, marker='s')
-		# plt.scatter(param[keys[ik]][arg_best], param[k][arg_best], c='r', s=50, marker='x')
+		plt.scatter(pX, pY, c=rC, cmap='CMRmap', vmin=0.0, vmax=np.max(p_W_act), s=5000, marker='s')
+		plt.scatter(param[keys[ik]][arg_best], param[k][arg_best], c='r', s=50, marker='x')
 		for i in range(len(pX)):
 			plt.text(pX[i], pY[i], str(np.round(rC[i]*100,1)), horizontalalignment='center', verticalalignment='center')
 		plt.xticks(pX)
@@ -67,4 +71,4 @@ for ik in range(len(keys)):
 		plt.tight_layout()
 		plt.savefig(folder_path + keys[ik] + '_' + k)
 
-plt.show()
+plt.show(block=False)
