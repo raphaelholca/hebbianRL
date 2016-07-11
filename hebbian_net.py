e377d13aa6275a4e226cdbaf358e624afc4280ee
Raphael Holca <raphaelholca@gmail.com>
2016-07-11 11:58:04 +0200

    removes old changes in plotting function for pypet

--- hebbian_net.py
+++ hebbian_net.py
@@ -299,7 +299,7 @@ class Network:
 					correct += np.sum(greedy==batch_labels)
 
 					#track ACh release
-					# self.ach_tracker[b*self.batch_size:(b+1)*self.batch_size, self._e] = ach_hid
+					if self.ach_release: self.ach_tracker[b*self.batch_size:(b+1)*self.batch_size, self._e] = ach_hid
 
 
 				#assess performance
