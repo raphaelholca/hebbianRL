fixes two bugs slowing down the code: 1- checking for correct weight assignment at each episode computing weighted average in stimuli-wise ach release

hebbian_net.py
@@ -694,7 +694,10 @@ class Network:
 		""" compute ach realease based on average performance for each stimulus """
 		if self._e >= self.n_epi_crit + self.n_epi_fine and self._e < self.n_epi_crit + self.n_epi_fine + self.n_epi_perc and self.ach_release: #ACh starts at perc
 		# if self.ach_release: #ACh starts at crit	
-			self._stim_perf_avg = ex.weighted_sum(self._stim_perf, self._stim_perf_weights)
+			if self.ach_stim and self.ach_uncertainty:
+				self._stim_perf_avg = np.mean(self._stim_perf)
+			else:
+				self._stim_perf_avg = ex.weighted_sum(self._stim_perf, self._stim_perf_weights)
 
 			if self.ach_func=='preset': #uses preset values (only valid of 1-4-9)
 				ach = np.ones_like(labels, dtype=float)
@@ -712,7 +715,7 @@ class Network:
 			else:
 				if self.ach_stim: #average over stimuli
 					if self.ach_uncertainty: #uses uncertainty of current stimulus
-						rel_perf = np.max(self.out_neurons_explore, axis=1)/np.mean(self._stim_perf_avg)
+						rel_perf = np.max(self.out_neurons_explore, axis=1)/self._stim_perf_avg
 					else:
 						perf_avg = self._stim_perf_avg[self._b*self.batch_size:(self._b+1)*self.batch_size]
 						rel_perf = perf_avg/np.mean(self._stim_perf_avg)
@@ -839,7 +842,7 @@ class Network:
 		""" assesses progression of performance of network as it is being trained """
 		
 		print_perf = 'epi ' + str(self._e) + ': '
-		if self.test_each_epi and (self._train_class_layer or self.classifier=='neural_prob'): ##remove neural_prob... 
+		if self.test_each_epi and self._train_class_layer: ##remove neural_prob... 
 			correct_out_W = self._check_out_W(images_train, labels_train)
 			print_perf += 'correct out weights: %d/%d ; ' %(correct_out_W, self.n_hid_neurons)
 		if self.test_each_epi and False: ## remove bool flag to measure likelihood at each episode

launch.py
@@ -40,13 +40,13 @@ net = hebbian_net.Network(	dHigh 				= 4.0,
 							ach_stim 			= True,
 							ach_uncertainty 	= True,
 							protocol			= 'digit', #'toy_data', 'gabor', 'digit'
-							name 				= 'ACh_uncertainty_stim_2',
+							name 				= 'ACh__uncertainty_stim_2D',
 							dopa_release 		= False, 
 							ach_release			= True, 
-							n_runs 				= 3,
-							n_epi_crit			= 30,
+							n_runs 				= 1,
+							n_epi_crit			= 5,
 							n_epi_fine 			= 0,
-							n_epi_perc			= 60,
+							n_epi_perc			= 0,
 							n_epi_post 			= 0,
 							t_hid				= 1.0,
 							t_out				= 0.1,
@@ -73,14 +73,14 @@ net = hebbian_net.Network(	dHigh 				= 4.0,
 							test_each_epi		= True,
 							early_stop 			= False,
 							verbose				= True,
-							seed 				= 984 #np.random.randint(1000)
+							seed 				= 983 #np.random.randint(1000)
 							)
 
 """ load and pre-process training and testing images """
 images_dict, labels_dict, ori_dict, images_params = ex.load_images(	protocol 		= net.protocol,
 																	A				= net.A,
 																	verbose 		= net.verbose,
-																	digit_params 	= {	'dataset_train'		: 'train',
+																	digit_params 	= {	'dataset_train'		: '2D',
 																						# 'classes' 			: np.array([ 0, 1, 3, 5, 8 ], dtype=int),
 																						'classes' 			: np.array([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ], dtype=int),
 																						'dataset_path' 		: '/Users/raphaelholca/Documents/data-sets/MNIST',
@@ -122,8 +122,6 @@ print '\nrun name:\t' + net.name
 print 'start time:\t' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(net._train_start))
 print 'end time:\t' + time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(net._train_stop))
 print 'train time:\t' +  str(datetime.timedelta(seconds=net.runtime))
-
-
 
 
 